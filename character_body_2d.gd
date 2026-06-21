63e8c0145f7490f925c7133f1f7a3864a7594fd1
CoinHawk9719255 <superthijsje@gmail.com>
2026-06-21 19:31:37 +0700

    move more

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
-
-@export var speed = 400
-
+#HAHA LARPSAN INGSAN'S A LARPER
+func larpsan(from: float, to: float, weight: float) -> float:
+	return lerp_angle(from, to, weight)
+	
+var speed = 400
+var rotation_speed = 2
+#move and look at mouse slowly i mean pretty self explanatory
+#ven 'I' who never coded in godot till this day made this
+#work with only a lilttle struggle and pain and suffering
+#and a complete lack of physics and degree radian nonsense
+#that i wouldve known if i actually listened in physics class
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	velocity = transform.x * Input.get_axis("down", "up") * speed
 
 func _physics_process(delta):
+	rotation = larpsan(rotation, (get_global_mouse_position()-global_position).angle()+deg_to_rad(0), rotation_speed*delta)
 	get_input()
 	move_and_slide()
