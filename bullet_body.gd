05518777284ae258951bcf1e63a8ddd4dc1d7f09
CoinHawk9719255 <superthijsje@gmail.com>
2026-06-23 15:12:09 +0700

    I HATE NIGHTS
    I HATE NIGHTS
    -Zoleil

--- bullet_body.gd
+++ bullet_body.gd
@@ -4,8 +4,12 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	linear_velocity = Vector2(100, 0)  # set initial velocity
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func apply_force_example():
+	apply_central_impulse(Vector2(0, -300))  # e.g. a jump/punch
--- rigid_body_2d.gd
+++ rigid_body_2d.gd
@@ -1,55 +1,45 @@
 extends RigidBody2D
 
 @onready var projectile := preload("res://bullet.tscn")
-@onready var target = get_node("../lowerboundcollisionwall/lower_bound") 
-@onready var pressedButton = false
-#@onready var bullet = get_node("../bullet_projectile").instantiate()
-@onready var bullet2new = projectile.instantiate()
+@onready var target = get_node("../lowerboundcollisionwall/lower_bound")
+
+var pressedButton = false
 var speed = 10.0
 var roll = false
+
 func _physics_process(_delta):
 	shoot_bullet()
 	barrel_roll()
 	var input = Input.get_axis("up", "down")
 	if input != 0.0:
-		apply_central_impulse(Vector2(0,input) * speed)
+		apply_central_impulse(Vector2(0, input) * speed)
 
 func _ready():
-	
 	$roll_cooldown.start()
 	body_entered.connect(_on_body_entered)
-	var newprojectile = projectile.instantiate()
-	add_child(newprojectile)
-
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "lower_bound":
 		print("touched: " + body.name)
-		
 		get_tree().change_scene_to_file("res://game_over_man_its_game_over.tscn")
 
 func barrel_roll():
 	if roll == true:
 		if Input.is_key_pressed(KEY_Q):
 			roll = false
-			apply_central_impulse(Vector2(0,200))
+			apply_central_impulse(Vector2(0, 200))
 			$roll_cooldown.start()
 			await get_tree().create_timer(1.25).timeout
-			apply_central_impulse(Vector2(0,-300))
+			apply_central_impulse(Vector2(0, -300))
 		elif Input.is_key_pressed(KEY_E):
 			roll = false
-			apply_central_impulse(Vector2(0,-200))
+			apply_central_impulse(Vector2(0, -200))
 			$roll_cooldown.start()
 			await get_tree().create_timer(1.25).timeout
-			apply_central_impulse(Vector2(0,280))
-		else:
-			pass
-	
-
+			apply_central_impulse(Vector2(0, 280))
 
 func _on_roll_cooldown_timeout() -> void:
 	roll = true
-
 
 #shooting shooting pew pew
 func shoot_bullet():
@@ -57,10 +47,10 @@ func shoot_bullet():
 		createBullet()
 		print("Hallo")
 		pressedButton = true
-		pass
 	elif not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		pressedButton = false
 
 func createBullet():
-	add_child(bullet2new)
-	pass
+	var newBullet = projectile.instantiate()  # NEW instance every shot
+	get_tree().current_scene.add_child(newBullet)
+	newBullet.global_position = global_position  # spawn at the gun's position
