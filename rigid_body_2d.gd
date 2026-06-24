17359ee06774a4879936333a97de11ed77def35a
CoinHawk9719255 <superthijsje@gmail.com>
2026-06-24 17:41:03 +0700

    Update rigid_body_2d.gd

--- rigid_body_2d.gd
+++ rigid_body_2d.gd
@@ -7,7 +7,7 @@ extends RigidBody2D
 @onready var speed = 10.0
 @onready var roll = false
 @onready var time_since_shot = fire_rate
-@export var ammo = 3
+@export var ammo = 300
 @onready var wepFuel = 300
 @onready var can_kamikaze = false
 @onready var canControl_plane = true
